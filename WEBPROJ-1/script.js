ed46d0d1db6296ea26f00524730cb18073cbb0d0
izumii <154342465+izumiCode@users.noreply.github.com>
2024-05-08 20:19:52 +0530

    style changes

--- WEBPROJ-1/script.js
+++ WEBPROJ-1/script.js
@@ -1,37 +1,37 @@
-var crsr=document.querySelector("#cursor")
-var blur=document.querySelector("#curblr")
-document.addEventListener("mousemove",function(dets){
-   crsr.style.left = dets.x+"px"
-   crsr.style.top = dets.y+"px"
-   blur.style.left = dets.x-150+"px"
-   blur.style.top = dets.y-150+"px"
-})
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#curblr");
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 150 + "px";
+  blur.style.top = dets.y - 150 + "px";
+});
 
+gsap.to("#nav", {
+  backgroundColor: "rgb(0,0,0,0.4)",
+  color: "white",
+  height: "60px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+  duration: "0.1",
+});
 
-
-gsap.to("#nav",{
-   // duration:0.2,
-   backgroundColor:"rgb(255,255,255,0.8)",
-   color:"blue",
-   height:"50px",
-   scrollTrigger:{
-      trigger:"#nav",
-      scroller:"body",
-      // markers:true,
-      start:"top -10%",
-      end:"top -11%",
-      scrub:2
-   }
-})
-
-gsap.to("#main",{
-   backgroundColor:"rgba(100,149,237,0.9)",
-   scrollTrigger:{
-      trigger:"#main",
-      scroller:"body",
-      // markers:true,
-      start:"top -65%",
-      end:"top -70%",
-      scrub:5
-   }
-})
+gsap.to("#main", {
+  backgroundColor: "rgba(0,0,0,0.6)",
+  backdropFilter: "blur(20px)",
+  color: "white",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers:true,
+    start: "top -65%",
+    end: "top -70%",
+    scrub: 1,
+  },
+});
